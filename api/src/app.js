0307bab1b0abf5dff33a6e96742d69fc7df283ef
Augusto <augusto.viggiano@gmail.com>
2023-07-26 03:57:59 -0300

    deploy commit

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "pokemonapp-api-production.up.railway.app/"
+    "*"
   ); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
